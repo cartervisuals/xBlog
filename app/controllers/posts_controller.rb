3c22ea0515467b50172fc8df210d6b9672f477d1
Ryan Carter <cartervisuals@gmail.com>
2015-06-02 17:15:36 -0600

    finished cats

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,49 @@
 class PostsController < ApplicationController
-  def index
+  
+  def new
+    @page_title = 'Add Post'
+    @post = Post.new
   end
 
-  def show
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = 'Post Created'
+      redirect_to admin_posts_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:notice] = "Post updated"
+      redirect_to admin_posts_path
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:notice] = "Post removed"
+    redirect_to admin_posts_path
+
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :category_id, :user_id, :tags, :body, :image)
   end
 end
