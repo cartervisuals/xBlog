2aee44a73886555a5fb923c07ae1bdf1454a3ca0
Ryan Carter <cartervisuals@gmail.com>
2015-06-03 14:21:05 -0600

    finished front end ui

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,9 @@
 class CategoriesController < ApplicationController
+
   def show
+    @category = Category.find(params[:id])
+    @categories = Category.all
+    @posts = @category.posts
   end
+
 end
