56f26e44b2104edbe4faa2f65c144e9dab903276
Ryan Carter <cartervisuals@gmail.com>
2015-06-02 20:58:44 -0600

    finished users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,28 @@
 class User < ActiveRecord::Base
 
+  attr_accessor :password
+  before_save :encrypt_password
+
   has_many :posts
 
   validates_presence_of :name
   validates_presence_of :email
-  validates_presence_of :password
-  validates :password, confirmation: true
+  validates :password, :presence => true, :confirmation => true
+
+  def self.authenticate(email, password)
+    user = find_by_email(email)
+    if user && user_password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+      user
+    else
+      nill
+    end
+  end
+
+  def encrypt_password
+    if password.present? 
+      self.password_salt = BCrypt::Engine.generate_salt
+      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+    end
+  end
 
 end
